66ec9ffffe598f0139fa4e3bbaf0e76a8f669d7f
tiffanyleesh74 <tiffanyleesh74@gmail.com>
2026-05-18 14:29:16 -0700

    change port number

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 8080;
 
 // Serve static files from the 'public' directory
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.listen(PORT, () => {
-    console.log(`Server is running at http://localhost:${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
